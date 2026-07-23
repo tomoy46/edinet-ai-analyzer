3a9a3171e254e710a58683c50a3597066fe8659f
tomoy46 <tomoya03212738@gmail.com>
2026-07-23 12:48:26 +0900

    ci: shift weekday disclosure update times

--- tests/test_update_workflow.py
+++ tests/test_update_workflow.py
@@ -10,10 +10,10 @@ class UpdateWorkflowTest(unittest.TestCase):
     def test_manual_and_weekday_schedules_are_configured(self):
         self.assertIn("  workflow_dispatch:\n", self.workflow)
         for cron in (
-            "0 0 * * 1-5",
-            "0 3 * * 1-5",
-            "30 6 * * 1-5",
-            "30 8 * * 1-5",
+            "7 0 * * 1-5",
+            "7 3 * * 1-5",
+            "37 6 * * 1-5",
+            "37 8 * * 1-5",
         ):
             self.assertEqual(self.workflow.count(f'- cron: "{cron}"'), 1)
 
